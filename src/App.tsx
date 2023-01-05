def1288c887575423d5a3aeac75fd527b21f9092
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-05 14:40:08 +0530

    fetching on reload done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 function App() {
   const url = "https://course-api.com/react-tabs-project";
 
   const [data, setData] = useState([]);
-  const fetching = async () => {
-    const response = await fetch(url);
-    const res = await response.json();
-    setData(res);
-  };
 
-  return <h1 onClick={fetching}>Hello</h1>;
+  useEffect(() => {
+    const fetching = async () => {
+      const response = await fetch(url);
+      const res = await response.json();
+      setData(res);
+    };
+    fetching();
+  }, []);
+
+  return <h1>Hello</h1>;
 }
 
 export default App;
