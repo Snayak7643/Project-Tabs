79a986d4bb19f1e8ac94c12ba9a6b04d8c759a9d
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-05 14:28:16 +0530

    Fetching complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 function App() {
-  return <h1>Hello</h1>;
+  const url = "https://course-api.com/react-tabs-project";
+
+  const [data, setData] = useState([]);
+  const fetching = async () => {
+    const response = await fetch(url);
+    const res = await response.json();
+    setData(res);
+  };
+
+  return <h1 onClick={fetching}>Hello</h1>;
 }
 
 export default App;
